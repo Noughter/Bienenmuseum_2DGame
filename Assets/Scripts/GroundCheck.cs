44b99ab08f4595faf800510a68ec03fe74cc3568
Noughter <marvin.ponten@gmx.de>
2017-06-10 17:35:55 +0200

    Basic Player + Movement Simpler Prototyp mit Spieler und Movement + Jumping

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+
+	private Player player; //referenzt Player Script
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = gameObject.GetComponentInParent<Player>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		player.grounded = true;
+	}
+
+	void OnTriggerStay2D(Collider2D col)	//Bugfix macht sicher das auch grounded bleibt
+	{
+		player.grounded = true;
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		player.grounded = false;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float maxSpeed;
+	public float speed;
+	public float jumpPower;
+
+	public bool grounded;
+
+	private Rigidbody2D rb2d;
+	private Animator anim;
+
+
+	// Use this for initialization 
+	void Start () {
+		rb2d = gameObject.GetComponent<Rigidbody2D> ();			//referneziert Objekte zu Unity Objekte
+		anim = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+		anim.SetBool ("Grounded", grounded); 				//referenziert script grounded mit Unity Animator Grounded
+		anim.SetFloat ("Speed",Mathf.Abs(rb2d.velocity.x));   //Math Abs lässt  nur Positiv zu und weist actual Speed zu
+
+
+		if (Input.GetAxis ("Horizontal") < -0.1f) 			//flippt Animation je nach Richtung
+		{
+			transform.localScale = new Vector3 (-1, 1, 1);
+		}
+
+		if (Input.GetAxis ("Horizontal") > 0.1f) 			
+		{
+			transform.localScale = new Vector3 (1, 1, 1);
+		}
+
+		if (Input.GetButtonDown("Jump") && grounded) 			
+		{
+			rb2d.AddForce (Vector2.up * jumpPower);
+		}
+
+	}
+
+	void FixedUpdate() 
+	{
+		float h = Input.GetAxis ("Horizontal");					// Holt gedrückten Key ( wenn links gedrückt wird wird Wert negativ)		
+
+		// Bewegt Spieler mit Keys
+		rb2d.AddForce ((Vector2.right * speed) * h);
+
+		//Limited Max Speed	
+		if (rb2d.velocity.x > maxSpeed) {		
+			rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+		}
+
+		if (rb2d.velocity.x < -maxSpeed) {											
+			rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+		}
+
+	}
+}
